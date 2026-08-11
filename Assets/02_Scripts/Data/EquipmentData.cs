6d7be2f976ea18900b73a4337cdfdc3d5ac1a238
김희준 <wnsl3462@nate.com>
2026-08-11 16:37:33 +0900

    feat: 성장 시스템 데이터 클래스 추가

---
+++ Assets/02_Scripts/Data/EquipmentData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+[Serializable]
+public class EquipmentData : BaseData
+{
+    public string Name;
+    public int GradeStar;
+    public float BaseAtk;
+    public float BaseHp;
+    public float BaseDef;
+    public float GrowthAtk;
+    public float GrowthHp;
+    public float GrowthDef;
+    public string SkillId;
+}
---
+++ Assets/02_Scripts/Data/SkillData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[Serializable]
+public class SkillData : BaseData
+{
+    public string Name;
+    public SkillType SkillType;
+    public int TargetCount;
+    public float CoolTime;
+    public float BaseEffect;
+    public float GrowthEffect;
+    public float BuffDuration;
+}
