bc550d406a205e36341dedff10ae5f2f6c41174e
김희준 <wnsl3462@nate.com>
2026-08-11 18:25:53 +0900

    feat: 동료 최종 스탯 계산 로직 구현

---
+++ Assets/02_Scripts/Character/GrowthSystem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GrowthSystem
+{
+    public CharacterStatData GetFinalStat(OwnedCompanionData ownedCompanion)
+    {
+        CompanionData companion = GameManager.DataTable.GetCompanionData(ownedCompanion.CompanionId);
+
+        if (companion == null)
+        {
+            Debug.LogError($"동료 데이터를 찾을 수 없습니다. Id: {ownedCompanion.CompanionId}");
+            return null;
+        }
+
+        float growthMultiplier = (ownedCompanion.Level - 1);
+        float finalAttack = companion.BaseAtk + (companion.GrowthAtk * growthMultiplier);
+        float finalDef = companion.BaseDef + (companion.GrowthDef * growthMultiplier);
+        float finalHp = companion.BaseHp + (companion.GrowthHp * growthMultiplier);
+
+        CharacterStatData finalStatData = new CharacterStatData(finalAttack, finalDef, finalHp);
+        
+        return finalStatData;
+    }
+}
