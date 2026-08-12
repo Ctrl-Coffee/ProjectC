42f6245511d89c5a5a569db2d10285b053d7e8b4
김희준 <wnsl3462@nate.com>
2026-08-12 18:24:54 +0900

    remove: SO 임시 로드 코드 제거

--- Assets/02_Scripts/Data/CompanionDataSO.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CompanionDataSO", menuName = "Scriptable Objects/CompanionDataSO")]
-public class CompanionDataSO : ScriptableObject
-{
-    public List<CompanionData> CompanionDataList;
-}
