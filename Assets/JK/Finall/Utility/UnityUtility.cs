09f2ba3ba7a311781705b33c3c9df45f67a4f2a0
이진규 <gdjinkus@gmail.com>
2026-08-14 14:17:43 +0900

    feat: 참조 유효성 검사 유틸 함수 추가

---
+++ Assets/JK/Finall/Utility/UnityUtility.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public static class UnityUtility
+{
+    public static bool ValidateReference(Object reference, string referenceName)
+    {
+        if (reference != null)
+        {
+            return true;
+        }
+
+        Debug.LogError($"'{referenceName}'가 할당되지 않았습니다.");
+        return false;
+    }
+}
