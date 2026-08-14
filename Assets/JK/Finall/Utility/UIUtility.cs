44d0d1e6ade2669b900ad131c35fd3364d046eda
이진규 <gdjinkus@gmail.com>
2026-08-14 22:03:20 +0900

    feat: 임시 스프라이트 로드 관련 유틸 함수 추가

---
+++ Assets/JK/Finall/Utility/UIUtility.cs
@@ -0,0 +1,24 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class UIUtility
+{
+    public static async UniTask SetSpriteAsync(Image image, string spriteKey)
+    {
+        if (image == null)
+        {
+            Debug.LogError($"전달된 이미지가 Null 입니다.");
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(spriteKey))
+        {
+            image.sprite = null;
+            return;
+        }
+
+        Sprite sprite = await ResourceUtility.LoadSpriteAsync(spriteKey);
+        image.sprite = sprite;
+    }
+}
