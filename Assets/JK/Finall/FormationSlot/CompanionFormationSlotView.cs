de2e2d5b7431345cccbb99d0306ae48579100d9f
이진규 <gdjinkus@gmail.com>
2026-08-14 14:19:52 +0900

    feat: 파티 편성 슬롯 관련 로직 개선
    - 공통으로 사용되는 로직을 별도 클래스로 분리하여 재사용성을 확보
    - 동료 슬롯은 공통 슬롯 클래스를 상속하여 슬롯별 고유 기능만 구현하도록 구조화

---
+++ Assets/JK/Finall/FormationSlot/CompanionFormationSlotView.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public sealed class CompanionFormationSlotView : FormationSlotView
+{
+    [SerializeField] private UIButtonComponent _slotButton;
+
+    private int _slotIndex;
+
+    public event Action<int> SlotClicked;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        UnityUtility.ValidateReference(_slotButton, nameof(_slotButton));
+    }
+
+    private void OnEnable()
+    {
+        _slotButton.BindButtonEvent(OnSlotClicked);
+    }
+
+    private void OnDisable()
+    {
+        _slotButton.UnBindButtonAllEvent();
+    }
+
+    public void Initialize(int slotIndex)
+    {
+        _slotIndex = slotIndex;
+    }
+
+    public void SetSelected(bool selected)
+    {
+        //선택 되었을 때 실행될 메서드
+    }
+
+    private void OnSlotClicked()
+    {
+        SlotClicked?.Invoke(_slotIndex);
+    }
+}
---
+++ Assets/JK/Finall/FormationSlot/FormationSlotView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FormationSlotView : MonoBehaviour
+{
+    [SerializeField] protected Image _formationImage;
+
+    protected virtual void Awake()
+    {
+        UnityUtility.ValidateReference(_formationImage, nameof(_formationImage));
+    }
+
+    public void SetSprite(Sprite sprite)
+    {
+        _formationImage.sprite = sprite;
+    }
+}
