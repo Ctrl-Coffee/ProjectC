0cba81c8085e97229e2743bf335f3f4e81d901e9
이진규 <gdjinkus@gmail.com>
2026-08-14 20:46:24 +0900

    feat: 동료 선택 목록 슬롯 개선
    - 동료 데이터 Id를 캐싱하도록 수정
    - 이벤트 변수에 직접 구독하도록 수정
    - 슬롯 초상화 이미지 추가

---
+++ Assets/JK/Finall/CompanionSelectionSlot/CompanionSelectionSlotView.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompanionSelectionSlotView : MonoBehaviour
+{
+    [SerializeField] private UIButtonComponent _slotButton;
+    [SerializeField] private Image _companionPortraitImage;
+    
+    private string _companionDataId;
+
+    public event Action<string> SlotClicked;
+
+    private void Awake()
+    {
+        UnityUtility.ValidateReference(_slotButton, nameof(_slotButton));
+        UnityUtility.ValidateReference(_companionPortraitImage, nameof(_companionPortraitImage));
+    }
+
+    private void OnEnable()
+    {
+        _slotButton.BindButtonEvent(OnSlotClicked);
+    }
+
+    private void OnDisable()
+    {
+        _slotButton.UnBindButtonAllEvent();
+    }
+
+    public void Initialize(string companionDataId)
+    {
+        _companionDataId = companionDataId;
+    }
+
+    public void SetSprite(Sprite sprite)
+    {
+        _companionPortraitImage.sprite = sprite;
+    }
+
+    public void OnSlotClicked()
+    {
+        SlotClicked?.Invoke(_companionDataId);
+    }
+}
