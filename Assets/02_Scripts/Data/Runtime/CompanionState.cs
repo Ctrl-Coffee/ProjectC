c6da31badfeb1d0a7d4a7787c1a07e9127bcdcd2
Kimikhwan <moonfgg@naver.com>
2026-08-16 01:28:43 +0900

    chore: 데이터 폴더 정리

---
+++ Assets/02_Scripts/Data/Runtime/CompanionState.cs
@@ -0,0 +1,11 @@
+﻿public class CompanionState
+{
+    public string CompanionId { get; }
+    public int Level { get; private set; }
+
+    public CompanionState(string companionId, int level)
+    {
+        CompanionId = companionId;
+        Level = level;
+    }
+}
