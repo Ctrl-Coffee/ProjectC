31ae0f617fd834a5e4197bd77bf1e58bee521bc1
Kimikhwan <moonfgg@naver.com>
2026-08-18 00:30:31 +0900

    feat: 드래그 앤 드랍

---
+++ Assets/02_Scripts/UI/NonMVVM/DraggableUI.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    // 이거 어떻게 가져오지???????
+    private Transform _rootCanvas; // content 캔버스가 되어야함.
+
+
+
+    private Transform _previousParent;
+    private RectTransform _rect;
+    private CanvasGroup _canvasGroup;
+
+    private void Awake()
+    {
+
+        _rect = GetComponent<RectTransform>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _previousParent = transform.parent;
+
+        transform.SetParent(_rootCanvas);
+        transform.SetAsLastSibling();
+
+        _canvasGroup.alpha = 0.6f;
+        _canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _rect.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (transform.parent == _rootCanvas)
+        {
+            transform.SetParent(_previousParent);
+            _rect.position = _previousParent.GetComponent<RectTransform>().position;
+        }
+
+        _canvasGroup.alpha = 1.0f;
+        _canvasGroup.blocksRaycasts = true;
+    }
+}
