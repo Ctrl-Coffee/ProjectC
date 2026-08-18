f46c5a0b4ab78ddc0db3a94dfb5ca9e441719601
이진규 <gdjinkus@gmail.com>
2026-08-18 20:30:24 +0900

    feat: 유닛별 애니메이션 클립 관리를 위한 SO 추가

---
+++ Assets/JK/Test/UnitAnimationSet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "UnitAnimationSet", menuName = "Scriptable Objects/UnitAnimationSet")]
+public class UnitAnimationSet : ScriptableObject
+{
+    [SerializeField] private AnimationClip _idle;
+    [SerializeField] private AnimationClip _basicAttack;
+    [SerializeField] private AnimationClip _skill;
+    [SerializeField] private AnimationClip _hit;
+    [SerializeField] private AnimationClip _dead;
+
+    public AnimationClip Idle { get { return _idle; } }  
+    public AnimationClip BasicAttack { get { return _basicAttack; } }
+    public AnimationClip Skill { get { return _skill; } }
+    public AnimationClip Hit { get { return _hit; } }
+    public AnimationClip Dead { get { return _dead; } }
+}
