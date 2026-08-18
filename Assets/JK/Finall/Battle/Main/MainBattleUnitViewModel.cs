458a937ea643ea8e7509c76cce30e2bba2283c4c
이진규 <gdjinkus@gmail.com>
2026-08-18 20:23:51 +0900

    feat: 메인 전투 유닛 뷰 추가

---
+++ Assets/JK/Finall/Battle/Main/MainBattleUnitViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+
+public class MainBattleUnitViewModel
+{
+    private readonly MainBattleUnitModel _mainBattleModel;
+
+    public event Action<string> PropertyChanged;
+
+    public MainBattleUnitViewModel()
+    {
+        _mainBattleModel = new MainBattleUnitModel();
+        _mainBattleModel.PropertyChanged += OnPropertyChanged;
+    }
+
+    public float Hp
+    {
+        get { return _mainBattleModel.Hp; }
+    }
+
+    public void Dispose()
+    {
+        _mainBattleModel.PropertyChanged -= OnPropertyChanged;
+    }
+
+    public void Initialize(string dataId)
+    {
+        _mainBattleModel.Initialize(dataId);
+    }
+
+    public void Refresh()
+    {
+        _mainBattleModel.InitializeOnce();
+    }
+
+    private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        PropertyChanged?.Invoke(e.PropertyName);
+    }
+}
