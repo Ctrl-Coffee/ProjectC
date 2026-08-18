cbfa3efa38cd9ccf69dd88f1ac27c3412764d41b
이진규 <gdjinkus@gmail.com>
2026-08-18 20:24:12 +0900

    feat: 적 전투 유닛 뷰 추가

---
+++ Assets/JK/Finall/Battle/Enemy/EnemyBattleUnitViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+
+public class EnemyBattleUnitViewModel
+{
+    private readonly EnemyBattleUnitModel _enemyBattleUnitModel;
+
+    public event Action<string> PropertyChanged;
+
+    public EnemyBattleUnitViewModel()
+    {
+        _enemyBattleUnitModel = new EnemyBattleUnitModel();
+        _enemyBattleUnitModel.PropertyChanged += OnPropertyChanged;
+    }
+
+    public float Hp
+    {
+        get { return _enemyBattleUnitModel.Hp; }
+    }
+
+    public void Dispose()
+    {
+        _enemyBattleUnitModel.PropertyChanged -= OnPropertyChanged;
+    }
+
+    public void Initialize(string dataId)
+    {
+        _enemyBattleUnitModel.Initialize(dataId);
+    }
+
+    public void Refresh()
+    {
+        _enemyBattleUnitModel.InitializeOnce();
+    }
+
+    private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        PropertyChanged?.Invoke(e.PropertyName);
+    }
+}
