3cb241cc6888578b12777209a57d7eda32d453e6
이진규 <gdjinkus@gmail.com>
2026-08-26 21:31:44 +0900

    feat: 전투 모델 초기화용 구조체 추가

---
+++ Assets/JK/Test/BattleUnitData.cs
@@ -0,0 +1,29 @@
+﻿public struct BattleUnitData
+{
+    public string UnitId;
+    public float MaxHp;
+    public float Attack;
+    public float Defense;
+    public float CriticalChance;
+    public float CriticalDamageMultiplier;
+    public float AttackSpeed;
+    public float CooldownReduction;
+    public string BasicAttackSkillId;
+    public string SignatureSkillId;
+    public string Key;
+
+    public BattleUnitData(string unitId, float maxHp, float attack, float defense, float criticalChance, float criticalDamageMultiplier, float attackSpeed, float cooldownReduction, string basicAttackSkillId, string signature, string animKey)
+    {
+        UnitId = unitId;
+        MaxHp = maxHp;
+        Attack = attack;
+        Defense = defense;
+        CriticalChance = criticalChance;
+        CriticalDamageMultiplier = criticalDamageMultiplier;
+        AttackSpeed = attackSpeed;
+        CooldownReduction = cooldownReduction;
+        BasicAttackSkillId = basicAttackSkillId;
+        SignatureSkillId = signature;
+        Key = animKey;
+    }
+}
