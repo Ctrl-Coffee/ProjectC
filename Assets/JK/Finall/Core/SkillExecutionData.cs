17377eb4e0b5c0a2c9535a238cd2db47f2fb9722
이진규 <gdjinkus@gmail.com>
2026-08-26 21:30:11 +0900

    feat: 전투 관련 구조체 추가

---
+++ Assets/JK/Finall/Core/SkillExecutionData.cs
@@ -0,0 +1,13 @@
+﻿public struct SkillExecutionData
+{
+    public float Attack;
+    public float CriticalChance;
+    public float CriticalDamageMultiplier;
+
+    public SkillExecutionData(float attack, float criticalChance, float criticalDamageMultiplier)
+    {
+        Attack = attack;
+        CriticalChance = criticalChance;
+        CriticalDamageMultiplier = criticalDamageMultiplier;
+    }
+}
