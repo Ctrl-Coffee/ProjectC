02fb80a330fa61cbbb80215243e47437237b1775
김경훈 <r930324@gmail.com>
2026-08-26 13:52:27 +0900

    feat: 기본 공격/스킬 가속 계산 로직 추가

--- Assets/02_Scripts/Utils/Const.cs
+++ Assets/02_Scripts/Utils/Const.cs
@@ -13,10 +13,14 @@
     public const float DEFENSE_WEIGHT = 10f;
     public const float NORMAL_SKILL_HASTE_WEIGHT = 5f;
     public const float SPECIAL_SKILL_HASTE_WEIGHT = 5f;
+    public const float HASTE_BASE = 100f;
+    public const float MIN_COOLDOWN_RATE = 0.01f;
 
     // 퍽 관련
     public const float NO_COMPOUND_RATE = 1f;
 
     // UI 문구, 추후 삭제
     public const string NO_PERK_BUFF = "적용 중인 퍽 효과가 없습니다.";
+    public const string COLOR_GOOD = "#7FD97F";
+    public const string COLOR_BAD = "#FF7B7B";
 }
