0e7ef888ca2a307ef6910e4a7ed8bc88c258bcdc
이진규 <gdjinkus@gmail.com>
2026-08-28 00:53:06 +0900

    feat: 전투 체력바 Hud 추가

---
+++ Assets/JK/UI/BattleUnitHpBarViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using UnityEngine;
+
+public class BattleUnitHpBarViewModel
+{
+    private BattleUnitModelBase _battleUnitModelBase;
+
+    public event Action<string> PropertyChanged;
+
+    public float MaxHp
+    {
+        get { return _battleUnitModelBase.MaxHp; }
+    }
+
+    public float Hp
+    {
+        get { return _battleUnitModelBase.Hp; }
+    }
+
+    public void SetModel(BattleUnitModelBase battleUnitModel)
+    {
+        if (battleUnitModel == null)
+        {
+            Debug.LogError("BattleUnitModel이 null입니다.");
+            return;
+        }
+
+        if (_battleUnitModelBase != null)
+        {
+            _battleUnitModelBase.PropertyChanged -= OnPropertyChanged;
+        }
+
+        _battleUnitModelBase = battleUnitModel;
+        _battleUnitModelBase.PropertyChanged += OnPropertyChanged;
+    }
+
+    public void Dispose()
+    {
+        if (_battleUnitModelBase == null)
+        {
+            return;
+        }
+
+        _battleUnitModelBase.PropertyChanged -= OnPropertyChanged;
+        _battleUnitModelBase = null;
+    }
+
+    private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        PropertyChanged?.Invoke(e.PropertyName);
+    }
+}
