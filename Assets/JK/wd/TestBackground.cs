674622b01afd711821424a4841ce15431259b602
이진규 <gdjinkus@gmail.com>
2026-08-31 14:01:19 +0900

    feat: 자동 전투 연출 로직 베이스 추가

---
+++ Assets/JK/wd/TestBackground.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class TestBackground : MonoBehaviour
+{
+    [SerializeField] private Transform[] _backgrounds;
+    [SerializeField] private float _scrollSpeed = 2f;
+    [SerializeField] private float _backgroundWidth = 20f;
+
+    private bool _isScrolling;
+
+    public void StartScroll()
+    {
+        _isScrolling = true;
+    }
+
+    public void StopScroll()
+    {
+        _isScrolling = false;
+    }
+
+
+    private void Start()
+    {
+        _isScrolling = true;
+    }
+
+    private void Update()
+    {
+        if (!_isScrolling)
+        {
+            return;
+        }
+
+        float movement = _scrollSpeed * Time.deltaTime;
+
+        foreach (Transform background in _backgrounds)
+        {
+            background.position += Vector3.left * movement;
+
+            if (background.position.x <= -_backgroundWidth)
+            {
+                MoveToEnd(background);
+            }
+        }
+    }
+
+    private void MoveToEnd(Transform background)
+    {
+        float rightmostX = float.MinValue;
+
+        foreach (Transform other in _backgrounds)
+        {
+            if (other == background)
+            {
+                continue;
+            }
+
+            rightmostX = Mathf.Max(
+                rightmostX,
+                other.position.x);
+        }
+
+        Vector3 position = background.position;
+        position.x = rightmostX + _backgroundWidth;
+
+        background.position = position;
+    }
+}
---
+++ Assets/JK/wd/TestUnit.cs
@@ -0,0 +1,88 @@
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+public class TestUnit : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _verticalRandomOffset = 0.3f;
+
+    private Vector3 _initialPosition;
+    private Tween _moveTween;
+
+    public void Initialize()
+    {
+        _initialPosition = transform.position;
+    }
+
+    public void PlayIdle()
+    {
+        _animator.Play("Idle");
+    }
+
+    public void PlayWalk()
+    {
+        _animator.Play("Walk");
+    }
+
+    public void PlayAttack()
+    {
+        _animator.Play("Attack");
+    }
+
+    public void PlayHit()
+    {
+        _animator.Play("Hit");
+    }
+
+    public void PlayDeath()
+    {
+        _animator.Play("Death");
+    }
+
+    public async UniTask MoveTo(
+        Vector3 targetPosition,
+        float duration)
+    {
+        _moveTween?.Kill();
+
+        _moveTween = transform
+            .DOMove(targetPosition, duration)
+            .SetEase(Ease.Linear);
+
+        await _moveTween.AsyncWaitForCompletion();
+    }
+
+    public async UniTask MoveLeft(
+        float distance,
+        float duration)
+    {
+        Vector3 targetPosition = transform.position;
+        targetPosition.x -= distance;
+
+        await MoveTo(targetPosition, duration);
+    }
+
+    public void ResetPosition()
+    {
+        _moveTween?.Kill();
+        _moveTween = null;
+
+        Vector3 position = _initialPosition;
+
+        position.y += Random.Range(-_verticalRandomOffset, _verticalRandomOffset);
+
+        transform.position = position;
+    }
+
+    public void Stop()
+    {
+        _moveTween?.Kill();
+        _moveTween = null;
+    }
+
+    private void OnDestroy()
+    {
+        _moveTween?.Kill();
+    }
+}
