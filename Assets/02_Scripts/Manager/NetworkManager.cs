b6d18dcb968a985a84997303926f85e3dc392509
Kimikhwan <moonfgg@naver.com>
2026-09-02 18:47:48 +0900

    fix: 배열 초기화 및 오타 수정

--- Assets/02_Scripts/Manager/NetworkManager.cs
+++ Assets/02_Scripts/Manager/NetworkManager.cs
@@ -257,10 +257,14 @@ public class NetworkManager
 
     public UniTask<SaveCompanionPartyResponse> SaveCompanionPartyAsync(string[] companionIds)
     {
-        CompanionPartyDto companionPartyDto = new();
-
-        companionPartyDto.companionIds[0] = companionIds[0];
-        companionPartyDto.companionIds[1] = companionIds[2];
+        CompanionPartyDto companionPartyDto = new()
+        {
+            companionIds = new string[2]
+            {
+                companionIds[0],
+                companionIds[1]
+            }
+        };
 
         CompanionPartyRequest request = new()
         {
